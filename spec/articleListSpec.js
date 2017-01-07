c0c4d58ed12fe4282b6bbe82d38b5b7fddc0fd1c
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-07 17:52:04 +0000

    Add tests to generate HTML for article list view

--- spec/articleListSpec.js
+++ spec/articleListSpec.js
@@ -5,18 +5,24 @@ function testArticleListHasArticles(){
 
 function testAddNewArticleToList(){
   var testArticleList = new ArticleList()
-  function testArticleDouble(){}
-  var testArticleDouble = new testArticleDouble()
+  function testArticleDouble(text){}
+  var testArticleDouble = new testArticleDouble("Test")
 
   testArticleList.addArticle(testArticleDouble)
   assert.isTrue(testArticleList.articles.length === 1)
 }
 
-// function testGenerateListHTML(){
-//   var testArticleList = new ArticleList()
-//   function testArticleDouble(){}
-//   var testArticleDouble = new testArticleDouble("Test Article")
-// }
+function testGenerateListHTML(){
+  var testArticleList = new ArticleList()
+  var ArticleDouble = function(title){ this.title = title }
+  var testArticleDouble = new ArticleDouble("Test2")
+  var testArticleDouble2 = new ArticleDouble("Test3")
+  testArticleList.addArticle(testArticleDouble)
+  testArticleList.addArticle(testArticleDouble2)
+
+  assert.isTrue(testArticleList.generateListHTML() === "<div><title>Test2</title></div><div><title>Test3</title></div>")
+}
 
 testArticleListHasArticles();
 testAddNewArticleToList();
+testGenerateListHTML();
