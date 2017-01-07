c441d5b0f7b5aba9a2abc50a36c9cd16d3ccca52
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-07 17:52:19 +0000

    Add method to generate HTML for article view

--- models/articleList.js
+++ models/articleList.js
@@ -8,6 +8,14 @@
     this.articles.push(article)
   }
 
+  ArticleList.prototype.generateListHTML = function(){
+    var output = ""
+    this.articles.forEach(function(article){
+      output += "<div><title>" + article.title + "</title></div>"
+    })
+    return output
+  }
+
   exports.ArticleList = ArticleList
 
 })(this);
