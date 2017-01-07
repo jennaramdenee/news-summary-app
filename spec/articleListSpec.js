38181f4ecdeb81a076797915f1ff23161a04d444
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-07 12:54:10 +0000

    Commented out tests for now

--- spec/articleListSpec.js
+++ spec/articleListSpec.js
@@ -5,12 +5,18 @@ function testArticleListHasArticles(){
 
 function testAddNewArticleToList(){
   var testArticleList = new ArticleList()
-  function testArticle(){}
-  var testArticle = new testArticle()
+  function testArticleDouble(){}
+  var testArticleDouble = new testArticleDouble()
 
-  testArticleList.addArticle(testArticle)
+  testArticleList.addArticle(testArticleDouble)
   assert.isTrue(testArticleList.articles.length === 1)
 }
+
+// function testGenerateListHTML(){
+//   var testArticleList = new ArticleList()
+//   function testArticleDouble(){}
+//   var testArticleDouble = new testArticleDouble("Test Article")
+// }
 
 testArticleListHasArticles();
 testAddNewArticleToList();
