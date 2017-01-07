7fbb0d9a80d6061f29ae8f11d55dc679353929cd
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-07 19:05:55 +0000

    Update with method to generate article id

--- models/articleList.js
+++ models/articleList.js
@@ -2,16 +2,22 @@
 
   var ArticleList = function(){
     this.articles = []
+    this.counter = 0
   }
 
   ArticleList.prototype.addArticle = function(article){
-    this.articles.push(article)
+    this.articles.push(new Article(article, id))
+  }
+
+  ArticleList.prototype.generateArticleId = function(){
+    this.counter += 1
+    return this.counter
   }
 
   ArticleList.prototype.generateListHTML = function(){
     var output = ""
     this.articles.forEach(function(article){
-      output += "<div><title>" + article.title + "</title></div>"
+      output += "<div><title><a href='#article/" + article.id + "'>" + article.title + "</a></title></div>"
     })
     return output
   }
--- spec/articleListSpec.js
+++ spec/articleListSpec.js
@@ -3,6 +3,17 @@ function testArticleListHasArticles(){
   assert.isTrue(testArticleList.hasOwnProperty("articles"))
 }
 
+function testArticleListHasCounter(){
+  var testArticleList = new ArticleList()
+  assert.isTrue(testArticleList.hasOwnProperty("counter"))
+}
+
+function testGenerateArticleId(){
+  var testArticleList = new ArticleList()
+  testArticleList.generateArticleId()
+  assert.isTrue(testArticleList.counter === 1)
+}
+
 function testAddNewArticleToList(){
   var testArticleList = new ArticleList()
   function testArticleDouble(text){}
@@ -14,15 +25,17 @@ function testAddNewArticleToList(){
 
 function testGenerateListHTML(){
   var testArticleList = new ArticleList()
-  var ArticleDouble = function(title){ this.title = title }
+  var ArticleDouble = function(title){ this.id = id, this.title = title}
   var testArticleDouble = new ArticleDouble("Test2")
   var testArticleDouble2 = new ArticleDouble("Test3")
   testArticleList.addArticle(testArticleDouble)
   testArticleList.addArticle(testArticleDouble2)
 
-  assert.isTrue(testArticleList.generateListHTML() === "<div><title>Test2</title></div><div><title>Test3</title></div>")
+  assert.isTrue(testArticleList.generateListHTML() === "<div><title><a href='#article/1'>Test2</a></title></div><div><title><a href='#article/2'>Test3</a></title></div>")
 }
 
 testArticleListHasArticles();
+testArticleListHasCounter();
+testGenerateArticleId();
 testAddNewArticleToList();
 testGenerateListHTML();
