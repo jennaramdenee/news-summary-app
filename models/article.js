36a2d723568646d49f2a64715b874ebd933ee20c
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-29 18:00:57 +0000

    Modified generateHTML method to use PureCSS syntax instead of bootstrap

--- models/article.js
+++ models/article.js
@@ -8,9 +8,11 @@
   }
 
   Article.prototype.generateArticleHTML = function(){
-    return "<div class='image'><img src='" + this.image + "'></div>"
+    return "<div class='pure-g'><div class='pure-u-1-2'>"
+      + "<div class='image'><img src='" + this.image + "'></div>"
       + "<div class='desc'>" + this.title + "</div>"
       + "<div class='content'>" + this.body + "</div>"
+      + "</div></div>"
   }
 
   exports.Article = Article;
--- spec/articleSpec.js
+++ spec/articleSpec.js
@@ -20,7 +20,7 @@ function testArticleHasImage(){
 
 function testGenerateArticleHTML(){
   var testArticle = new Article("Test Title", "Test Body", "Test Image");
-  assert.isTrue(testArticle.generateArticleHTML() === "<div class='image'><img src='Test Image'></div><div class='desc'>Test Title</div><div class='content'>Test Body</div>")
+  assert.isTrue(testArticle.generateArticleHTML() === "<div class='pure-g'><div class='pure-u-1-2'><div class='image'><img src='Test Image'></div><div class='desc'>Test Title</div><div class='content'>Test Body</div></div></div>")
 }
 
 
