13df82eed383353ebad91880312874afaac3aad2
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-29 17:24:28 +0000

    Modified generateArticleID method to return an integer instead of a string

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -1,13 +1,39 @@
 (function(exports){
 
-  var ArticleController = function(articleList){
-    this.articleList = articleList
-    this.articleListView = new ArticleListView(articleList)
+  var ArticleController = function(){
+    this.articleList = new ArticleList()
+    this.articleListView = new ArticleListView(this.articleList)
   }
 
-  ArticleController.prototype.initialLoad = function(){
+  ArticleController.prototype.initialLoad = function(list){
+    var articles = this.articleList
+    list.forEach(function(article){
+      articles.addArticle(article)
+    })
     this.articleListView.updateArticleListHTML()
   }
+
+  ArticleController.prototype.getIdFromUrl = function(location){
+    return parseInt(location.hash.split("#article/")[1])
+  }
+
+  ArticleController.prototype.findArticleFromId = function(id){
+    var result;
+    this.articleList.articles.forEach(function(article){
+      if (article.id === id ){
+        result = article
+      }
+    })
+    return result
+  }
+
+  ArticleController.prototype.showArticle = function(){
+    var article = this.findArticleFromId(this.getIdFromUrl(window.location))
+    var articleView = new ArticleView(article)
+    articleView.updateArticleHTML()
+  }
+
+
 
   exports.ArticleController = ArticleController
 
