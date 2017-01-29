56b3e99eb1a58c7a35a898a448dc228bba915506
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-01-29 17:26:27 +0000

    Modified generateArticleID method to return an integer instead of a string

--- spec/articleControllerSpec.js
+++ spec/articleControllerSpec.js
@@ -1,6 +1,6 @@
 function testControllerHasListModel(){
   function testArticleListDouble(){}
-  var testArticleController = new ArticleController(testArticleListDouble)
+  var testArticleController = new ArticleController()
   assert.isTrue(testArticleController.hasOwnProperty("articleList"))
 }
 
@@ -9,5 +9,39 @@ function testControllerCreatesNewView(){
   assert.isTrue(testArticleController.hasOwnProperty("articleListView"))
 }
 
+function testGetIdFromUrl(){
+  var testArticleController = new ArticleController()
+  var testlocation = window.location
+  testlocation.hash = "/#article/1"
+  assert.isTrue(testArticleController.getIdFromUrl(testlocation) === 1)
+}
+
+function testFindArticleFromId(){
+  var testArticleController = new ArticleController()
+  function ArticleDouble(){ this.id = 2 }
+  var testArticleDouble = new ArticleDouble()
+  function ArticleListDouble(){ this.articles = [testArticleDouble]}
+  var testArticleList = new ArticleListDouble();
+  testArticleController.articleList = testArticleList
+  assert.isTrue(testArticleController.findArticleFromId(2) === testArticleDouble)
+}
+
+function testShowArticle(){
+  var testArticleController = new ArticleController()
+  var testlocation = window.location
+  testlocation.hash = "/#article/2"
+
+  function ArticleDouble(){ this.id = 2 }
+  var testArticleDouble = new ArticleDouble()
+
+  function ArticleListDouble(){ this.articles = [testArticleDouble]}
+  var testArticleList = new ArticleListDouble();
+  testArticleController.articleList = testArticleList
+  assert.isTrue(testArticleController.showArticle() === testArticleDouble)
+}
+
 testControllerHasListModel();
 testControllerCreatesNewView();
+testGetIdFromUrl();
+testFindArticleFromId();
+// testShowArticle();
